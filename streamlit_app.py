7e8f5a6f3d4d0edfe8b9af019cd11157ca59a3fe
acdesazilla-sketch <acdesazilla@gmail.com>
2026-05-22 14:20:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,12 @@ st.header("Two", divider=True)
 st.header("Three", divider=True)
 st.header("Four", divider=True)
 import streamlit as st
+import streamlit as st
+import time
+
+with st.empty():
+    for seconds in range(10):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write(":material/check: 10 seconds over!")
+st.button("Rerun")
